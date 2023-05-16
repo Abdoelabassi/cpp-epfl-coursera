1289dac40118553bc725623b79117a40967e2a92
Abdoelabassi <abderrezaq@outlook.com>
2023-05-16 19:56:54 +0100

    date paques complete

--- assignement-4/date_paques.cc
+++ assignement-4/date_paques.cc
@@ -11,7 +11,7 @@ int demander_annee(){
         cout << "Entrez une annee (1583-4000) :";
         cin >> annee;
 
-    }while(not(annee>=1583 and annee<=4000));
+    }while(not(annee>1583 and annee<4000));
 
     return annee;
 }
@@ -28,6 +28,29 @@ void affiche_date(int annee, int nombre){
             cout << nombre_jour << " avril ";
         }
     }
+
+}
+
+int date_paques(int annee){
+    
+    int siecle = annee/100;
+    int p = (13 + 8*siecle)/25;
+    int q = siecle/4;
+    int M = (15 - p + siecle - q)% 30;
+    int N = (4+siecle-q)%7;
+    int d = (M+19*(annee%19))%30;
+    int e = (2*(annee%4) + 4*(annee%7) + 6+d + N )%7;
+    int day = e + d + 22;
+
+    if((e==6) and ((d==29) or ((d==28) and (11*(M+1)%30 < 19)))){
+        
+        day -= 7;
+
+    }
+
+    return day;
+
+
 }
 
 int main(){
