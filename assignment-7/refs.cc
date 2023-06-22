1b47aed36b0a1543223c699dc0ed4cc3601a8e3a
Abdoelabassi <abderrezaq@outlook.com>
2023-06-22 13:31:19 +0100

    week7 refs

---
+++ assignment-7/refs.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+struct Maison {
+
+    string addresse;
+
+};
+
+struct Personne {
+
+    string nom;
+    Maison& masion;
+
+};
+
+void affiche(const Personne& p){
+
+    cout << p.nom << " habite " << p.masion.addresse << endl;
+
+    
+
+
+}
+
+
+
+int main()
+{
+
+    Maison m1 = {"104 bulvard Fes"};
+    Maison m2 = {"192 avenue Ouad EL Makahzine"};
+
+    Personne jefferson = {"Jefferson", m1};
+    Personne katie = {"Katie", m2};
+
+    affiche(jefferson); affiche(katie);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
