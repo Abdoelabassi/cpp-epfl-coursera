aaa16a21d3a65533f1fbd241353eaa17f81a06a7
Abdoelabassi <abderrezaq@outlook.com>
2023-08-09 19:12:33 +0100

    to be continued

--- cpp_oop/week2/banque1.cc
+++ cpp_oop/week2/banque1.cc
@@ -3,6 +3,17 @@
 using namespace std;
 
 
+// global vars 
+struct Interet{
+
+  double interet1;
+  double interet2;
+          
+
+};
+
+
+
 // Compte
 
 class Compte{
@@ -62,11 +73,65 @@ class Client{
           // Cette fonction affiche les données d'un client
           void afficherClient(double solde1, double solde2) {
                 Compte compte(solde1, solde2);
-                cout << "Client " << nom << " de " << ville << endl;
+                cout << "Client " << name << " de " << ville << endl;
                 cout << "   Compte courant : " << compte.getSoldePrive() << " francs" << endl;
                 cout << "   Compte épargne : " << compte.getSoldeEparagne() << " francs" << endl;
           }
 
+          std::string getName(){
+            return name;
+          }
+
+          std::string getVille(){
+            return ville;
+          }
+
+
+
+};
+
+class Billet{
+
+    private:
+      double taux1;
+      double taux2;
+
+    public:
+      Billet(double taux1, double taux2)
+        : taux1(taux1), taux2(taux2)
+        {
+          std::cout << "definit les taux d'interet" << std::endl;
+        }
+      ~Billet(){std::cout << "fin de calcule" << std::endl;}
+
+      Billet(Billet const& billet)
+        : taux1(billet.taux1), taux2(billet.taux2)
+        {
+          std::cout << "billet duplique" << std::endl;
+        }
+
+        double getPTaux(){
+          return taux1;
+        }
+
+        double getDTaux(){
+          return taux2;
+        }
+
+        
+
+        // Cette fonction ajoute les intérêts au solde d'un compte
+        Interet bouclerCompte(Compte &compte, Billet &taux) {
+
+                Interet Int;
+                
+                Int.interet1 = taux.getPTaux() * compte.getSoldePrive();
+                Int.interet1 += compte.getSoldePrive();
+                Int.interet2 = taux.getDTaux()* compte.getSoldeEparagne();
+                Int.interet2 += compte.getSoldeEparagne();
+                return Int;
+                
+        }
 
 
 };
@@ -78,12 +143,7 @@ class Client{
 
 
 
-// Cette fonction ajoute les intérêts au solde d'un compte
-double bouclerCompte(double solde, double taux) {
-  double interets(taux * solde);
-  double nouveauSolde(solde + interets);
-  return nouveauSolde;
-}
+
 
 int main()
 {
@@ -91,6 +151,8 @@ int main()
   double taux1(0.01);
   // Données pour tous les comptes d'épargne (taux d'intérêt) :
   double taux2(0.02);
+
+  Billet Taux(0.01, 0.02);
   // Données pour le premier client (nom et ville) :
   Client client1("Pedro", "Geneve");
   // Données pour le compte privé du premier client (solde) :
@@ -108,20 +170,16 @@ int main()
   // Afficher les données du deuxième client:
   client2.afficherClient(compte2.getSoldePrive(), compte2.getSoldeEparagne());
 
-  // Bouclement du compte privé du premier client:
-  solde1PremierClient = bouclerCompte(solde1PremierClient, taux1);
-  // Bouclement du compte d'épargne du premier client:
-  solde2PremierClient = bouclerCompte(solde2PremierClient, taux2);
-  // Bouclement du compte privé du deuxième client:
-  solde1DeuxiemeClient = bouclerCompte(solde1DeuxiemeClient, taux1);
-  // Bouclement du compte d'épargne du deuxième client:
-  solde2DeuxiemeClient = bouclerCompte(solde2DeuxiemeClient, taux2);
+  // Bouclement du compte privé et epargne du premier client:
+  Interet soldePremierClient = Taux.bouclerCompte(compte1, Taux);
+  // Bouclement du compte privé et epargne du deuxième client:
+  Interet soldeDeuxiemeClient = Taux.bouclerCompte(compte2, Taux);
 
   cout << "Données apres le bouclement des comptes :" << endl;
   // Afficher les données du premier client:
-  afficherClient(nom1, ville1, solde1PremierClient, solde2PremierClient);
+  client1.afficherClient(compte1.getSoldePrive(), compte1.getSoldeEparagne());
   // Afficher les données du deuxième client:
-  afficherClient(nom2, ville2, solde1DeuxiemeClient, solde2DeuxiemeClient);
+  client2.afficherClient(compte2.getSoldePrive(), compte2.getSoldeEparagne());
 
   return 0;
 }
