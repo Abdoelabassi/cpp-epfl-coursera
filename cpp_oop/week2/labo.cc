ee0fbd89f030e07e061da02188c0a0330ff02c4e
Abdoelabassi <abderrezaq@outlook.com>
2023-08-25 22:56:22 +0100

    assign: ex1 passed the checker

--- cpp_oop/week2/labo.cc
+++ cpp_oop/week2/labo.cc
@@ -5,18 +5,18 @@ using namespace std;
 class Souris
 {
   /*****************************************************
-    ComplÃ©ter le code Ã  partir d'ici
-  *******************************************************/
+    Complèter le code à  partir d'ici
+  *******************************************************/ 
 public:
 
-    Souris(double poids, string colour, unsigned int age = 0, unsigned int esperance_vie = 36)
-        : poids(poids), colour(colour), age(age), esperance_vie(esperance_vie)
+    Souris(double poids, string couleur, unsigned int age = 0, unsigned int esperance_vie = 36)
+        : poids(poids), couleur(couleur), age(age), esperance_vie(esperance_vie)
         {
             cout << "Une nouvelle souris !" << endl;
             clonee = false;
         }
     Souris(Souris const& souris)
-     : poids(souris.poids), colour(souris.colour), age(souris.age), esperance_vie(4*souris.esperance_vie/5)
+     : poids(souris.poids), couleur(souris.couleur), age(souris.age), esperance_vie(4*souris.esperance_vie/5)
      {
         cout << "Clonage d'une souris ! " << endl;
         clonee = true;
@@ -30,37 +30,37 @@ public:
 
      void afficher(){
         if(clonee){
-            cout << "Une souris " << colour << ", clonee, " << "de  " << age << " mois et pesant " << poids << " grammes" << endl;
+            cout << "Une souris " << couleur << ", clonee, " << "de  " << age << " mois et pesant " << poids << " grammes" << endl;
         }else{
-            cout << "Une souris " << colour << " de " << age << " mois et pesant " << poids << " grammes" << endl;
+            cout << "Une souris " << couleur << " de " << age << " mois et pesant " << poids << " grammes" << endl;
         }
      }
 
      void vieillir(){
-        if(clonee){
+        if(clonee && age > esperance_vie /2){
             age += 1;
+            couleur = "verte";
         }
-        if(age > esperance_vie / 2 ){
-            colour = "verte";
-        }
+       
      }
 
      void evolue(){
 
         while(age < esperance_vie){
             age +=1;
+            vieillir();
         }
 
      }
 
 private:
     double poids;
-    string colour;
+    string couleur;
     unsigned int age;
     unsigned int esperance_vie;
     bool clonee;
   /*******************************************
-   * Ne rien modifier aprÃ¨s cette ligne.
+   * Ne rien modifier aprés cette ligne.
    *******************************************/
 
 }; // fin de la classe Souris
@@ -70,7 +70,7 @@ int main()
   Souris s1(50.0, "blanche", 2);
   Souris s2(45.0, "grise");
   Souris s3(s2);
-  // ... un tableau peut-Ãªtre...
+  // ... un tableau peut-être...
   s1.afficher();
   s2.afficher();
   s3.afficher();
