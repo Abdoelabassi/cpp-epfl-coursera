12c00e1fc04df620a9f515448d238d787467b36c
Abdoelabassi <abderrezaq@outlook.com>
2023-09-02 19:51:56 +0100

    week 3: operator overloading

--- cpp_oop/week3/polynome.cc
+++ cpp_oop/week3/polynome.cc
@@ -25,3 +25,35 @@ class Polynome
         void affiche_coef(ostream&, Degre,bool) const;
 
 };
+
+
+void Polynome::affiche_coef(ostream& out, Degre puissance, bool signe) const 
+{ double const c(p[puissance]); 
+if (c != 0)
+{ 
+    if (signe and (c > 0.0)) out << "+"; 
+out << c; 
+if (puissance > 1) 
+out << "*X^" << puissance; 
+else if (puissance == 1) 
+out << "*X"; 
+}else if (degre() == 0) 
+{ // degré 0 : afficher quand meme le 0 si rien d'autre 
+out << 0; 
+} 
+}
+ // ---------------------------------------------------------------------- 
+ ostream& operator<<(ostream& sortie, const Polynome& polynome) { 
+    // plus haut degré : pas de signe + devant 
+    Degre i(polynome.degre()); 
+    polynome.affiche_coef(sortie, i, false);
+    // degré de N à 0 : +a*X^i 
+    if (i > 0) 
+    { 
+        for (i--; i > 0; i--) polynome.affiche_coef(sortie, i, true); 
+        polynome.affiche_coef(sortie, 0, false); 
+    }
+ 
+ return sortie; 
+ }
+ 
