0479e9b268cf2746d6e6b692fde810b716a1355b
Abdoelabassi <abderrezaq@outlook.com>
2023-09-23 23:11:39 +0100

    heritage practice

--- cpp_oop/week4/Vehicule.cc
+++ cpp_oop/week4/Vehicule.cc
@@ -7,8 +7,10 @@ using namespace std;
 class Vehicule
 {
     private:
-        string marque;
         unsigned int date;
+
+    protected:
+        string marque;
         double prix;
 
     public:
@@ -16,8 +18,13 @@ class Vehicule
             : marque(marque_), date(date_), prix(prix_) {}
 
         void affiche(ostream& sortie) const {
-            sortie << "La marque " << marque << ", date d'achat " << date << ", prix " << prix;
+            sortie << "La marque: " << marque << ", date d'achat: " << date << ", prix d'achat: " << prix;
         }
+
+    void calculPrix()
+    {
+        prix /= 0.001;
+    }
 
 
 };
@@ -36,9 +43,38 @@ class Voiture : public Vehicule
             {}
         void affiche(ostream& sortie)
         {
+            sortie << "---------Voiture----------" << endl;
             Vehicule::affiche(sortie);
             sortie << endl;
-            sortie << cylindree << " Litres " << nombre_de_portes << " portes " << puissance << " CV " << kilometrage << " Km. ";
+            sortie << cylindree << " Litres " << nombre_de_portes << " portes " << puissance << " CV " << kilometrage << " Km. " << endl;
+        }
+
+        void calculPrix()
+        {
+            if (prix < 0.0)
+            {
+                prix = 0.0;
+            }
+            else if (kilometrage >= 10000)
+            {
+                prix /= 0.05;
+
+            }
+            else if (marque == "Fiat" || marque == "Renault")
+            {
+                prix /= 0.1;
+            }
+            else if (marque == "Ferrari" || marque == "Porsche")
+            {
+                prix /= 0.2;
+
+            }
+            else
+            {
+                prix /= 0.02;
+            }
+
+            cout << "prix actuel: " << prix;
         }
 
 };
@@ -56,9 +92,71 @@ class Avion : public Vehicule
 
         void affiche(ostream& sortie)
         {
+            sortie << "-------Avion a " << type << "-----------" << endl;
             Vehicule::affiche(sortie);
             sortie << endl;
-            sortie << nombre_heure_vol << " heures de vol ";
+            sortie << nombre_heure_vol << " heures de vol " << endl;
+        }
+
+        void calculPrix()
+        {
+            if (prix < 0.0)
+            {
+                prix = 0.0;
+            }
+            else if (nombre_heure_vol == 1000)
+            {
+                prix /= 0.1;
+            }
+            else if (nombre_heure_vol == 100)
+            {
+                prix /= 0.1;
+            }
+
+            cout << "prix actuel: " << prix;
+           
         }
 
 };
+
+int main()
+{
+
+  vector<Voiture> garage;
+  vector<Avion>   hangar;
+ 
+  string REACTION = "REACTION";
+  string HELICES = "HELICES";
+
+
+  garage.push_back(Voiture("Peugeot", 1998, 147325.79, 2.5, 5, 180.0,
+                           12000));
+  garage.push_back(Voiture("Porsche", 1985, 250000.00, 6.5, 2, 280.0,
+                           81320));
+  garage.push_back(Voiture("Fiat",    2001,   7327.30, 1.6, 3,  65.0, 
+                           3000));
+  hangar.push_back(Avion("Cessna",      1972, 1230673.90, HELICES,  
+                         250));
+  hangar.push_back(Avion("Nain Connu", 1992, 4321098.00, REACTION,
+                         1300));
+
+
+  for (auto voiture : garage)
+  {
+    voiture.calculPrix();
+    voiture.affiche(cout);
+  }
+
+  for (auto avion : hangar)
+  {
+    avion.calculPrix();
+    avion.affiche(cout);
+  }
+
+
+
+
+
+
+    return 0;
+}
