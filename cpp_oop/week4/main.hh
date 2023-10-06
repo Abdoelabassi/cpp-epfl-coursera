39433c849b242d7cb4c0ae51fc5a60914c348094
Abdoelabassi <abderrezaq@outlook.com>
2023-10-06 17:55:07 +0100

    running tests

--- cpp_oop/week4/main.hh
+++ cpp_oop/week4/main.hh
@@ -1,6 +1,64 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <iostream>
+#include <string>
+#include <cmath>
 
+using namespace std;
+
+int distance(int x, int y)
+{
+    return abs(x - y);
+}
+
+
+
+class Creature
+{
+    protected:
+    const string nom_;
+    int niveau_;
+    int points_de_vie_;
+    int force_;
+    int position_;
+
+    public:
+        Creature(const string nom, int niveau, int points_de_vie, int force, int position = 0);
+
+        bool vivant() const;
+        int points_attaque() const;
+        void deplacer(int x);
+        void audieux() const;
+        int position() const;
+        void faiblir(int x);
+        void afficher() const;
+};
+
+class Dragon : public Creature
+{
+    private:
+        int portee_flammee_;
+    public:
+        Dragon(const string nom, int niveau, int points_de_vie, int force, int portee_flamme, int position = 0);
+        
+        void voler(int pos);
+
+        void souffle_sur(Creature& bete);
+
+};
+
+class Hydre : public Creature
+{
+    private:
+        int longueur_cou_;
+        int dose_poison_;
+
+    public:
+        Hydre(const string nom, int niveau, int points_de_vie , int force, int longueur_cou, int dose_posion, int position = 0);
+
+        void empoisonne(Creature& bete);
+
+};
 
 #endif
--- cpp_oop/week4/test.cc
+++ cpp_oop/week4/test.cc
@@ -1,1 +1,119 @@
 #include "main.hh"
+
+Creature::Creature(const string nom, int niveau, int points_de_vie, int force, int position = 0)
+{}
+
+bool Creature::vivant() const
+{
+     if (points_de_vie_ > 0)
+      {
+        return true;
+      }
+      else return false;
+}
+
+int Creature::points_attaque() const
+{
+    if (vivant() == true)
+      {
+        return niveau_ * force_;
+      }
+
+      else return 0;
+}
+void Creature::deplacer(int x)
+{
+    position_ += x;
+}
+
+void Creature::audieux() const
+{
+    cout << nom_ << " n'est plus! " << endl;
+}
+
+int Creature::position() const
+{
+    return position_;
+}
+
+void Creature::faiblir(int x)
+{
+    if (vivant() == true)
+      {
+        points_de_vie_ -= x;
+        if (points_de_vie_ < 0)
+        {
+          points_de_vie_ = 0;
+        }
+
+      }
+      if (vivant() == false)
+      {
+        points_de_vie_ = 0;
+        audieux();
+      }
+}
+
+void Creature::afficher() const
+{
+    cout << nom_ << ", niveau: " << niveau_ << ", points de vie: " << points_de_vie_ << ", force: " << force_ << ", points d'attaque: " << points_attaque() << ", position: " << position_ << endl;
+}
+
+Dragon::Dragon(const string nom, int niveau, int points_de_vie, int force, int portee_flamme, int position = 0)
+    : Creature(nom, niveau, points_de_vie, force, position), portee_flammee_(portee_flamme)
+{}
+
+void Dragon::voler(int x)
+{
+    deplacer(x);
+}
+
+void Dragon::souffle_sur(Creature& bete)
+{
+    int d(distance(bete.position(), this->position()));
+      if (this->vivant() == true && bete.vivant() == true)
+      {
+        if (portee_flammee_ >= d)
+        {
+          bete.faiblir(this->points_attaque());
+          this->faiblir(d);
+        }
+        if (bete.vivant() == false && this->vivant() == true)
+        {
+          this->niveau_++;
+        }
+      }
+}
+
+Hydre::Hydre(const string nom, int niveau, int points_de_vie , int force, int longueur_cou, int dose_posion, int position = 0)
+    : Creature(nom, niveau, points_de_vie, force, position), longueur_cou_(longueur_cou), dose_poison_(dose_posion)
+{}
+
+void Hydre::empoisonne(Creature& bete)
+{
+    int d = distance(bete.position(), this->position());
+        if (this->vivant() == true && bete.vivant() == true)
+        {
+         if (longueur_cou_ >= d)
+         {
+            bete.faiblir(this->points_attaque() + this->dose_poison_);
+         }
+         if (bete.vivant() == false && this->vivant() == true)
+         {
+          this->niveau_++;
+         }
+        }
+}
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
+
+
