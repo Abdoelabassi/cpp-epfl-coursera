efe075fb74d6c9da7284112cfaffd36a9e9e3435
Abdoelabassi <abderrezaq@outlook.com>
2023-11-07 21:46:29 +0100

    multiple inheritance

---
+++ cpp_oop/week6/zoo.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+
+class Animal
+{
+    ~Animal(){cout << "You called Animal destructor " << endl;}
+
+};
+
+class Vivipare
+{
+    
+    public:
+        Vivipare(unsigned int duree_gestion_ = 128) : duree_gestion(duree_gestion_)
+        {
+            cout << "Vivipare called for duty!." << endl;
+        }
+    void naissance() const
+    {
+        cout << "Après " << duree_gestion << " jours de duree_gestion, je viens de mettre au monde un nouveau bébé." << endl;
+    }
+
+    protected:
+        unsigned int duree_gestion;
+
+
+};
+
+class Ovipare
+{
+    protected:
+        unsigned int n_ouef;
+    public:
+        Ovipare(unsigned int ouef = 2): n_ouef(ouef)
+        {
+            cout << "Ovipare called for duty!" << endl;
+        }
+    void naissance() const
+    {
+        cout << "Je viens de pondre environ " << n_ouef << " oeuf(s)." << endl;
+    }
+
+
+};
+
+class Ovovivipare: public Vivipare, public Ovipare
+{
+    public:
+        Ovovivipare(unsigned int jours, unsigned int n_oeuf , bool rare_ = false)
+            : Vivipare(jours), Ovipare(n_oeuf) ,rare(rare_) 
+            {
+                cout << "Ovovivipare called for duty!." << endl;
+            }
+
+        
+        void naissance() const
+        {
+            cout << "Après " << duree_gestion << " jours de gestation, je viens de mettre au monde " << n_ouef << " nouveau(x) bébé(s)." << endl;
+        }
+        
+
+    protected:
+        bool rare;
+
+};
+
+int main()
+{
+    Ovovivipare o(200, 6);
+    o.naissance();
+
+
+
+
+
+    return 0;
+
+}
